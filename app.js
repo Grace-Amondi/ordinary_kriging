9f759fab7bc4bc6bd83d5c61f43df74e11d5a437
Grace-Amondi <ochieng.grace@students.jkuat.ac.ke>
2020-04-02 22:37:23 +0300

    train model form

--- app.js
+++ app.js
@@ -14,28 +14,21 @@
 // var xnew = [-1.2544011203660779, -1.2331201247997963, -1.301767871939471, -1.2770548931316255, -1.2317471514699085]
 // var ynew = [36.74446105957031, 36.82823181152344, 36.72111511230469, 36.802825927734375, 36.802825927734375];
 
-// // predict variables for new coordinates 
-// function predict(x, y, variogram) {
-//     var i, k = Array(variogram.n);
-//     for (i = 0; i < variogram.n; i++) {
-//         k[i] = variogram.model(Math.pow(Math.pow(x - variogram.x[i], 2) +
-//             Math.pow(y - variogram.y[i], 2), 0.5),
-//             variogram.nugget, variogram.range,
-//             variogram.sill, variogram.A);
-//     }
+// predict variables for new coordinates 
+function predict(x, y, variogram) {
+    var i, k = Array(variogram.n);
+    for (i = 0; i < variogram.n; i++) {
+        k[i] = variogram.model(Math.pow(Math.pow(x - variogram.x[i], 2) +
+            Math.pow(y - variogram.y[i], 2), 0.5),
+            variogram.nugget, variogram.range,
+            variogram.sill, variogram.A);
+    }
 
-//     return kriging_matrix_multiply(k, variogram.M, 1, variogram.n, 1)[0];
-// };
+    return kriging_matrix_multiply(k, variogram.M, 1, variogram.n, 1)[0];
+};
 
 
-// // var xnew, ynew /* Pair of new coordinates to predict */;
-// var predictedVal = []
-// for (var i = 0; i < variogram.n; i++) {
-//     var tpredicted = predict(xnew[i], ynew[i], variogram);
-//     predictedVal.push(tpredicted)
-// }
 
-// console.log(predictedVal)
 
 
 // map view 
@@ -49,6 +42,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZ3JhY2VhbW9uZGkiLCJhIjoiY2poampha2g1MDQ5czNkcXplMzMycGJtYyJ9.uec448K2BkM1FADfN4YA9Q'
 }).addTo(map);
+$('select').material_select();
 
 // for(var i=0;i<variogram.n;i++){
 //     L.marker([variogram.x[i],variogram.y[i]]).addTo(map);
@@ -91,21 +85,69 @@ $(".button-collapse").sideNav();
 $('.tap-target').tapTarget('open');
 $('.tap-target').tapTarget('close');
 
-var radios = document.getElementsByName("group1")
 var sigmaRange = document.getElementById("test9")
+
+
+
+
+function trainDataForm(event) {
+    event.preventDefault()
+
+    // console.log(uploadTrainingData())
+    var input = document.getElementById("train_data")
+    // var input = event.target;
+    var reader = new FileReader();
+    reader.readAsText(input.files[0]);
+    reader.onload = fileLoaded
+
+
+
+}
+
+var fileLoaded = function (event) {
+
+    var userInput = $(".trainForm").serializeArray()
+    // var variable = userInput[0].value
+    var model = userInput[1].value
+    var sigma2 = userInput[2].value
+    var alpha = userInput[3].value
+    var dataURL = event.target.result;
+    var trainingGeojson = JSON.parse(dataURL)
+    var t = []
+    var x = []
+    var y = []
+
+    console.log(Object.keys(trainingGeojson.features[0].properties)[0])
+
+
+    for (var i = 0; i < trainingGeojson.features.length; i++) {
+        var copper = trainingGeojson.features[i].properties.copper
+        var X = trainingGeojson.features[i].geometry.coordinates[1]
+        var Y = trainingGeojson.features[i].geometry.coordinates[0]
+
+        t.push(copper)
+        x.push(X)
+        y.push(Y)
+    }
+
+
+    var variogram = kriging.train(t, x, y, model, sigma2, alpha);
+    console.log(variogram)
+
+    return dataURL
+    // train model 
+    // console.log(dataURL)
+}
+
+
+fileLoaded(onload, function () {
+    console.log(variogram)
+})
 
 sigmaRange.addEventListener("change", function (event) {
     console.log(event.target.value)
 })
 
-
-
-
-// var radios = document.forms["formA"].elements["myradio"];
-
-// const selectedFile = document.getElementById('train_data').files[0];
-// var reader = new FileReader();
-// var fileToRead = document.querySelector('train_data').files[0];
 
 function fileRead() {
     var input = event.target;
@@ -117,7 +159,6 @@ function fileRead() {
         var trainingLayer = L.geoJSON(trainingGeojson)
         var addTrainingLayer = trainingLayer.addTo(map)
         var variableOptions = Object.keys(trainingGeojson.features[0].properties)[0]
-        $('select').material_select();
         var variables = $('select')["0"]
         console.log($('select')["0"].innerHTML)
 
@@ -130,66 +171,48 @@ function fileRead() {
         // console.log(reader.result)
         return reader.result
     };
-
     reader.readAsText(input.files[0]);
     // return reader
 
 }
 
-
-
 function uploadTrainingData(event) {
     fileRead(event)
 
 };
-
-// console.log(uploadTrainingData(onsubmit))
-
-function trainData(event) {
-
-    // console.log(uploadTrainingData())
-    event.preventDefault()
-    var input = document.getElementById("train_data")
-    // var input = event.target;
+function predictData(event) {
+    console.log(trainDataForm(event))
+    uploadTestData(event)
+    // // var xnew, ynew /* Pair of new coordinates to predict */;
+    // var predictedVal = []
+    // for (var i = 0; i < variogram.n; i++) {
+    //     var tpredicted = predict(xnew[i], ynew[i], variogram);
+    //     predictedVal.push(tpredicted)
+    // }
+    // console.log(predictedVal)
+}
+function uploadTestData(event) {
+    var input = event.target;
     var reader = new FileReader();
-    reader.readAsText(input.files[0]);
+
     reader.onload = function () {
         var dataURL = reader.result;
         var trainingGeojson = JSON.parse(dataURL)
-        var t = []
-        var x = []
-        var y = []
-        
-        console.log(Object.keys(trainingGeojson.features[0].properties)[0])
+        var trainingLayer = L.geoJSON(trainingGeojson)
+        var addTrainingLayer = trainingLayer.addTo(map)
+        var variableOptions = Object.keys(trainingGeojson.features[0].properties)[0]
 
 
-        for (var i = 0; i < trainingGeojson.features.length; i++) {
-            var copper = trainingGeojson.features[i].properties.copper
-            var X = trainingGeojson.features[i].geometry.coordinates[1]
-            var Y = trainingGeojson.features[i].geometry.coordinates[0]
 
-            t.push(copper)
-            x.push(X)
-            y.push(Y)
-        }
+        console.log(variables)
 
-        var sigma2 = 0, alpha = 10;
-        var model = "gaussian"
-        // for (var i = 0, max = radios.length; i < max; i++) {
-        //     radios[i].onclick = function () {
-        //         var model = this.value
-        //         var variogram = kriging.train(t, x, y, model, sigma2, alpha);
+        map.fitBounds(trainingLayer.getBounds());
 
-        //     }
-        // }
-        var variogram = kriging.train(t, x, y, model, sigma2, alpha);
-
-        // train model 
-        console.log(variogram)
-    }
+        // console.log(reader.result)
+        return reader.result
+    };
+    reader.readAsText(input.files[0]);
 }
-
-
 
 // download predicted data 
 function saveToFile(content, filename) {
