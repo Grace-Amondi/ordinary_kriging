e10663741a735d0f670afa3d3b3cb112e2d520d9
Grace-Amondi <ochieng.grace@students.jkuat.ac.ke>
2020-04-08 02:03:13 +0300

    moved kriging function into index.js

--- js/index.js
+++ js/index.js
@@ -14,6 +14,150 @@ var map = new mapboxgl.Map({
     center: [36.74446105957031, -1.2544011203660779],
     zoom: 9
 });
+var kriging = {};
+// Train using gaussian processes with bayesian priors
+kriging.train = function (t, x, y, model, sigma2, alpha) {
+    var variogram = {
+        t: t,
+        x: x,
+        y: y,
+        nugget: 0.0,
+        range: 0.0,
+        sill: 0.0,
+        A: 1 / 3,
+        n: 0
+    };
+    switch (model) {
+        case "gaussian":
+            variogram.model = kriging_variogram_gaussian;
+            break;
+        case "exponential":
+            variogram.model = kriging_variogram_exponential;
+            break;
+        case "spherical":
+            variogram.model = kriging_variogram_spherical;
+            break;
+    };
+
+    // Lag distance/semivariance
+    var i, j, k, l, n = t.length;
+    var distance = Array((n * n - n) / 2);
+    for (i = 0, k = 0; i < n; i++)
+        for (j = 0; j < i; j++, k++) {
+            distance[k] = Array(2);
+            distance[k][0] = Math.pow(
+                Math.pow(x[i] - x[j], 2) +
+                Math.pow(y[i] - y[j], 2), 0.5);
+            distance[k][1] = Math.abs(t[i] - t[j]);
+        }
+    distance.sort(function (a, b) { return a[0] - b[0]; });
+    variogram.range = distance[(n * n - n) / 2 - 1][0];
+
+    // Bin lag distance
+    var lags = ((n * n - n) / 2) > 30 ? 30 : (n * n - n) / 2;
+    var tolerance = variogram.range / lags;
+    var lag = [0].rep(lags);
+    var semi = [0].rep(lags);
+    if (lags < 30) {
+        for (l = 0; l < lags; l++) {
+            lag[l] = distance[l][0];
+            semi[l] = distance[l][1];
+        }
+    }
+    else {
+        for (i = 0, j = 0, k = 0, l = 0; i < lags && j < ((n * n - n) / 2); i++, k = 0) {
+            while (distance[j][0] <= ((i + 1) * tolerance)) {
+                lag[l] += distance[j][0];
+                semi[l] += distance[j][1];
+                j++; k++;
+                if (j >= ((n * n - n) / 2)) break;
+            }
+            if (k > 0) {
+                lag[l] /= k;
+                semi[l] /= k;
+                l++;
+            }
+        }
+        if (l < 2) return variogram; // Error: Not enough points
+    }
+
+    // Feature transformation
+    n = l;
+    variogram.range = lag[n - 1] - lag[0];
+    var X = [1].rep(2 * n);
+    var Y = Array(n);
+    var A = variogram.A;
+    for (i = 0; i < n; i++) {
+        switch (model) {
+            case "gaussian":
+                X[i * 2 + 1] = 1.0 - Math.exp(-(1.0 / A) * Math.pow(lag[i] / variogram.range, 2));
+                break;
+            case "exponential":
+                X[i * 2 + 1] = 1.0 - Math.exp(-(1.0 / A) * lag[i] / variogram.range);
+                break;
+            case "spherical":
+                X[i * 2 + 1] = 1.5 * (lag[i] / variogram.range) -
+                    0.5 * Math.pow(lag[i] / variogram.range, 3);
+                break;
+        };
+        Y[i] = semi[i];
+    }
+
+    // Least squares
+    var Xt = kriging_matrix_transpose(X, n, 2);
+    var Z = kriging_matrix_multiply(Xt, X, 2, n, 2);
+    Z = kriging_matrix_add(Z, kriging_matrix_diag(1 / alpha, 2), 2, 2);
+    var cloneZ = Z.slice(0);
+    if (kriging_matrix_chol(Z, 2))
+        kriging_matrix_chol2inv(Z, 2);
+    else {
+        kriging_matrix_solve(cloneZ, 2);
+        Z = cloneZ;
+    }
+    var W = kriging_matrix_multiply(kriging_matrix_multiply(Z, Xt, 2, 2, n), Y, 2, n, 1);
+
+    // Variogram parameters
+    variogram.nugget = W[0];
+    variogram.sill = W[1] * variogram.range + variogram.nugget;
+    variogram.n = x.length;
+
+    // Gram matrix with prior
+    n = x.length;
+    var K = Array(n * n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < i; j++) {
+            K[i * n + j] = variogram.model(Math.pow(Math.pow(x[i] - x[j], 2) +
+                Math.pow(y[i] - y[j], 2), 0.5),
+                variogram.nugget,
+                variogram.range,
+                variogram.sill,
+                variogram.A);
+            K[j * n + i] = K[i * n + j];
+        }
+        K[i * n + i] = variogram.model(0, variogram.nugget,
+            variogram.range,
+            variogram.sill,
+            variogram.A);
+    }
+
+    // Inverse penalized Gram matrix projected to target vector
+    var C = kriging_matrix_add(K, kriging_matrix_diag(sigma2, n), n, n);
+    var cloneC = C.slice(0);
+    if (kriging_matrix_chol(C, n))
+        kriging_matrix_chol2inv(C, n);
+    else {
+        kriging_matrix_solve(cloneC, n);
+        C = cloneC;
+    }
+
+    // Copy unprojected inverted matrix as K
+    var K = C.slice(0);
+    var M = kriging_matrix_multiply(C, t, n, n, 1);
+    variogram.K = K;
+    variogram.M = M;
+
+    return variogram;
+};
 
 // train incoming data 
 function train(t, x, y, model, sigma2, alpha) {
